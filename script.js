1e66d97a46c445945ec904a954e61da488eac36c
Josh B <bowenpjoshua@gmail.com>
2023-08-20 23:01:06 -0400

    updated javascript and css for final design. design could use some tweaking but the interactions with boostrap are causing headaches, look into this at later time.

--- script.js
+++ script.js
@@ -1,30 +1,52 @@
-class timeBlock {
-    constructor(hours, todo) {
-        this.hour = hours;
-        this.todo = todo;
+$(document).ready(function () {// script for load order
+    //moment for time
+    $("#currentDay").text(moment().format("MMM D, YY - h:mm a"));
+    $(".saveBtn").on("click", function () {
+        
+        console.log(this);
+        var abc = $(this).siblings(".description").val(); 
+        var time = $(this).parent().attr("id"); 
+
+        //set items in local storage.
+        localStorage.setItem(time, abc);
+    })
+    //load local storage. should also fix 24 - 12 hour
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+    function hourTracker() {
+        //get hours
+        var currentHour = moment().hour();
+
+        // loop over time blocks
+        $(".time-block").each(function () {
+            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+            console.log( blockHour, currentHour)
+
+            //change color for past present and future
+            if (blockHour < currentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+            }
+            else if (blockHour === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
     }
-}
-
-window.onload = function() {
-    const currentTime = getCurrentTime();
-    const time = moment();
-
-    showCurrentDay(time);
-    showTime(time);
-
-    document.querySelector('.cal')
-    .addEventListener('click', function(event) {
-        containerClicked(event, currentTime);
-    });
-    setTimeText(currentTime);
-};
-
-function getCurrentTime() {
-    const currentTime = localStorage.getItem('timeObj');
-    return currentTime ? JSON.parse(currentTime) : [];
-}
-
-function showCurrentDay(time) {
-    document.getElementById('today')
-    .textContent = time.format('dddd, MMMM Do');
-}
+    hourTracker(); //re-run function
+})
